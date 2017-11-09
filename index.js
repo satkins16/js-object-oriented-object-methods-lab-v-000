d5b869c745387365b57242ab8874d5d6261cd6f5
Scott Atkins <satkins16@gmail.com>
2017-11-09 15:07:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ function BoardMember(name, homeState, training) {
     this.homeState = homeState
     this.training = training
 
-    
+    this.veto = function() {
+      console.log('No, I must disagree')
+    }
 }
