523ecee86db5ad0c0eb2fb1d1588ce0f488a0434
Scott Atkins <satkins16@gmail.com>
2017-11-09 15:10:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,7 @@ function BoardMember(name, homeState, training) {
     this.releasePressStatement = function() {
       return 'You will see great things from Scuber.'
     }
+    this.sayHi = function( {
+      return `Hi, my name is ${this.name}. I am from ${this.hometown}, and I was trained in ${this.training}.`
+    })
 }
